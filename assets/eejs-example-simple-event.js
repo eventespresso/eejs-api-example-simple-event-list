72a3989c5dafb8e223adb9f2a54c5504807a1b48
Darren Ethier <darren@roughsmootheng.in>
2017-02-24 15:34:33 -0500

    add example for optimization

--- assets/eejs-example-simple-event.js
+++ assets/eejs-example-simple-event.js
@@ -25,3 +25,65 @@
         console.log(e);
     });
 })();
+
+/**
+ * Alternative more optimized version
+ *
+ * This version will result in the Event endpoint only being hit ONCE for the event vs twice in the original.
+ * Note: you'd also need to make this change to the component template as defined in the SimpleEvent::shortcodeContent method found in SimpleEvent.php to replace the current return line there.
+ *
+ *  return EEH_Template::display_template(
+         EEJS_EXAMPLE_PLUGIN_DIR . 'templates/app_wrapper_container.php',
+         array( 'component' => '<event-form v-bind:initial-id="event.EVT_ID"></event-form><event-preview v-bind:initial-id="event.EVT_ID"></event-preview>' ),
+         true
+    );
+ *
+ */
+/**
+ (function(){
+    'use strict';
+    eejs.api.init({collections:['events']}).then(function(){
+        //uniqId = eejs.utils.getUniqueId(),
+        var Event = eejs.vue.extend(eejs.api.components.event),
+            EventPreview = Event.extend({
+                template: eejs.data.templates.single_event_preview,
+                skipInitialization : true,
+                initialIdSet : false,
+                watch : {
+                    'initialId' : function(value) {
+                        if (! this.$options.initialIdSet) {
+                            this.id = value;
+                            this.fetchById();
+                            this.$options.initialIdSet = true;
+                        }
+                    }
+                }
+            }),
+            EventForm = Event.extend({
+                template: eejs.data.templates.single_event_form,
+                skipInitialization : true,
+                initialIdSet : false,
+                watch : {
+                    'initialId' : function(value) {
+                        if (! this.$options.initialIdSet) {
+                            this.id = value;
+                            this.fetchById();
+                            this.$options.initialIdSet = true;
+                        }
+                    }
+                }
+            });
+
+       var eventView = new Event({
+           el: '#app',
+           id: eejs.data.event_id,
+           components : {
+               'eventForm' : EventForm,
+               'eventPreview' : EventPreview
+           }
+       });
+    }).catch(function(e){
+        console.log(e);
+    });
+})();
+ /**/
