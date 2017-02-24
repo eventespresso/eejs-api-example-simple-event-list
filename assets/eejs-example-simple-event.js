dd365e3df15e5024295e2a0b78c62ada97aa3007
Darren Ethier <darren@roughsmootheng.in>
2017-02-24 11:40:04 -0500

    add in commented out sample code for event creation

--- assets/eejs-example-simple-event.js
+++ assets/eejs-example-simple-event.js
@@ -2,12 +2,15 @@
     'use strict';
     eejs.api.init({collections:['events']}).then(function(){
         var Event = eejs.vue.extend(eejs.api.components.event),
+            //uniqId = eejs.utils.getUniqueId(),
             EventPreview = Event.extend({
                 template: eejs.data.templates.single_event_preview,
+                // id : uniqId
                 id : eejs.data.event_id
             }),
             EventForm = Event.extend({
                 template: eejs.data.templates.single_event_form,
+                // id : uniqId
                 id : eejs.data.event_id
             });
 
